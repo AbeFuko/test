b8695a48aaaf2da4eca9ada669d46995f6227cb4
maifujii <b1100136@gl.aiu.ac.jp>
2019-02-20 21:50:52 +0900

    2019/02/20

--- ecsite/sql/ecsite.sql
+++ ecsite/sql/ecsite.sql
@@ -27,3 +27,20 @@ create table item_info_transaction(
 	insert_date datetime,
 	update_date datetime
 );
+
+drop table if exists user_buy_item_transaction;
+
+create table user_buy_item_transaction(
+	id int not null primary key auto_increment,
+	item_transaction_id int,
+	total_price int,
+	total_count int,
+	user_master_id varchar(16),
+	pay varchar(30),
+	insert_date datetime,
+	delete_date datetime
+);
+
+insert into item_info_transaction(item_name, item_price, item_stock) values("Notebook", 100, 50);
+
+insert into login_user_transaction(login_id, login_pass, user_name) values("internous", "internous01", "test");
